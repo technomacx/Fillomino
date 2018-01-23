0561f844670dd7a0daa3bc3b1092c583de654c80
technomacx <technomacx@gmail.com>
2018-01-23 14:13:06 +0200

    orginazation things

---
+++ src/Node.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fillomino;
+
+
+/**
+ *
+ * @author ahmad
+ */
+public class Node {
+
+
+    public int value = 0;
+    public boolean visited = false, changeAble = true;
+    int x = 0;
+    int y = 0;
+
+    public Node(int val, boolean visit, int x, int y) {
+        value = val;
+        visited = visit;
+        this.x = x;
+        this.y = y;
+    }
+}
